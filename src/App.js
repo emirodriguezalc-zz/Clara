b85e338d39772a71c8be653082afed1dc1e257d0
Emilia Rodriguez Gimenez <emilia.rodriguez@mercadolibre.com>
2020-03-18 21:46:09 -0300

    Autoreview

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Second from './pages/second';
 import Third from './pages/third';
 
 function App() {
-
+  //Esto habria que ver de modificarlo para que no sea necesario pasar un array de objetos
   const itemsObj = [
     { title: "First", search: "first", component: First },
     { title: "Second", search: "second", component: Second },
